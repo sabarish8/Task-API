b998d4f097a219708791fe82ea21b626a643f174
sabarish8 <160460565+sabarish8@users.noreply.github.com>
2024-03-29 13:41:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from"react";
+import './App.css';
+import Fetchapp from "./Fetchapp";
+
+function App() {
+  return (
+    <div className="App">
+      <Fetchapp/>
+    </div>
+  );
+}
+
+export default App;
